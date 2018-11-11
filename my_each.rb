57b84037804fc97728f0f2d6d2db501f1cf0506a
Meisuleen Kased <meisuleen@gmail.com>
2018-11-11 18:42:30 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,10 +2,11 @@ require 'pry'
 def my_each(array) # put argument(s) here
   # code here
   my_each.tas do |ta|
-    put array
+    puts array
   i = 0
   while i < array.length
     yield array[i]
     i = i + 1
+  end
   end
 end
