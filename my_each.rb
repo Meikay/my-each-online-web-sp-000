779a0aa8945d4b93a9f82a9d71a531ad9939a4d0
Meisuleen Kased <meisuleen@gmail.com>
2018-11-11 18:37:36 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,11 @@
 require 'pry'
 def my_each(array) # put argument(s) here
   # code here
+  binding.pry
   i = 0
   while i < array.length
     yield array[i]
     i = i + 1
-    binding.pry
+    
   end
 end
